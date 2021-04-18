14a15aba28e3d8a42104417ea3d97025f62065bb
zjalicf <64900037+zjalicf@users.noreply.github.com>
2021-04-18 23:38:50 +0200

    added mapsAPI

---
+++ src/main/main.java
@@ -0,0 +1,11 @@
+package main;
+
+import mapsAPI.mapsCalc;
+
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        mapsCalc.proba();
+    }
+}
---
+++ src/mapsAPI/mapsCalc.java
@@ -0,0 +1,30 @@
+package mapsAPI;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class mapsCalc {
+        public static void proba() throws IOException {
+            URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" +
+                    "place_id:EidWb2p2b8SRYW5za2loIGJyaWdhZGEsIE5vdmkgU2FkLCBTZXJiaWEiLiosChQKEgnZuNCGaxBbRxFhZCd4" +
+                    "ufM_" + "RBIUChIJVTTpPWEQW0cRKP4kN2h9bws&destinations=place_id:ChIJM_fE1mYQW0cRA3ReiVkHSTY&ke" +
+                    "y=AIzaSyAP1ILAhEyPnYCXt4EDQfnlTr-Dm5riBnw");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            String line, outputString = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = reader.readLine()) != null) { outputString += line; }
+            JSONObject obj = new JSONObject(outputString);
+            JSONObject rows = obj.getJSONArray("rows").getJSONObject(0);
+            JSONArray elements = rows.getJSONArray("elements");
+            String distance = elements.getJSONObject(0).getJSONObject("distance").getString("text");
+            String time = elements.getJSONObject(0).getJSONObject("duration").getString("text");
+            System.out.println("Razdaljina je: " + distance);
+            System.out.println("Vreme je: " + time);
+        }
+}
