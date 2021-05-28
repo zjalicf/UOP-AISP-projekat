98328c4df2de6a9d1c93e7fa028d0629d417d6a6
zjalicf <64900037+zjalicf@users.noreply.github.com>
2021-05-28 20:24:16 +0200

    github pravi problem oko main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import GUI.FrameLauncher;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         FrameLauncher.launchLoginFrame();
     }
 }
